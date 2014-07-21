02d564b1eeca7b5c241795ec0ea9d2f833b5fd9f
Alexander B Aa <alexander@aasebo.net>
2014-07-21 23:57:24 +0200

    Removed files not in use.

--- registrate.php
+++ registrate.php
@@ -1,6 +1,13 @@
 <?php
 include 'inc/startup.php';
 
-$VIS->addBCpath('registrate.php', 'Registration', 'This is where you can register a new user.');
-$VIS->activateBC();
-$VIS->createBreadCrumbs();
+?>
+
+<form action="registrate.php" method="post">
+    <input type="text" name="email" value="" placeholder="Email" /> <br>
+    <input type="text" name="fname" value="" placeholder="First Name" /> <br>
+    <input type="text" name="lname" value="" placeholder="Last Name" /> <br>
+    <input type="text" name="username" value="" placeholder="Username" /> <br>
+    <input type="password" name="password" value="" placeholder="Password" /> <br>
+    <input type="password" name="repassword" value="" placeholder="Repeat password" /> <br>
+</form>
